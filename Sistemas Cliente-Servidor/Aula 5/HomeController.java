29703d650feb125637558db27017ed3f7b716561
JNCJcoder <jorgenettocjunior@gmail.com>
2022-11-13 16:12:58 -0300

    Sistemas Cliente-Servidor - Aula 5

---
+++ Sistemas Cliente-Servidor/Aula 5/HomeController.java
@@ -0,0 +1,19 @@
+package com.cruzeiro;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController
+{
+    public HomeController()
+    {
+
+    }
+
+    @RequestMapping("/")
+    public String showPage()
+    {
+        return "main-menu";
+    }
+}
---
+++ Sistemas Cliente-Servidor/Aula 5/main-menu.jsp
@@ -0,0 +1,11 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1" pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="ISO-8859-1">
+    <title>Exemplo Spring MVC</title>
+</head>
+<body>
+    <h1>Exemplo de Spring MVC - Menu Principal</h1>
+</body>
+</html>
